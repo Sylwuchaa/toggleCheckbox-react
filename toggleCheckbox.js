2c1e1603d3c553fbe6bbced47cf30e4aed2bcf2f
Sylwcuhaa <uzumagi1@gmail.com>
2018-12-27 16:09:11 +0100

    validation form added

--- toggleCheckbox.js
+++ toggleCheckbox.js
@@ -1,34 +1,60 @@
-const PositiveMessage = () => <p>Możesz obejrzeć film, Zapraszamy</p>
-const NegativeMessage = () =>  <p>Nie możesz obejrzeć tego filmu jeśli nie masz więcej niż 16 lat</p>
+const PositiveMessage = () => <p>Możesz obejrzeć film, Zapraszamy</p>;
+const NegativeMessage = () => (
+  <p>Nie możesz obejrzeć tego filmu jeśli nie masz więcej niż 16 lat</p>
+);
 
 class ToggleCheckbox extends React.Component {
   state = {
     isConfirmed: false,
-  }
+    isFormSubmitted: false
+  };
   handleCheckboxChange = () => {
     this.setState({
-      isConfirmed: !this.state.isConfirmed
-    })
-  }
-  displayMessage = () => {
-    if (this.state.isConfirmed) {
-      return <PositiveMessage />
-    } else  {
-      return <NegativeMessage />
+      isConfirmed: !this.state.isConfirmed,
+      isFormSubmitted: false,
+    });
+  };
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (!this.state.isFormSubmitted) {
+      this.setState({
+        isFormSubmitted: true
+      });
     }
-  }
+  };
+  displayMessage = () => {
+    if (this.state.isFormSubmitted) {
+      if (this.state.isConfirmed) {
+        return <PositiveMessage />;
+      } else {
+        return <NegativeMessage />;
+      }
+    } else {
+      return null;
+    }
+  };
 
   render() {
-    console.log(this.state.isConfirmed);
-    return(
+    // console.log(this.state.isConfirmed);
+    return (
       <>
-        <h1>Bilet na horror roku</h1>
-        <input type="checkbox" id="age" onChange={this.handleCheckboxChange} checked={this.state.isConfirmed}/>
-        <label htmlFor="age">Mam co najmniej 16 lat</label>
+        <form onSubmit={this.handleFormSubmit}>
+          <h1>Bilet na horror roku</h1>
+          <input
+            type="checkbox"
+            id="age"
+            onChange={this.handleCheckboxChange}
+            checked={this.state.isConfirmed}
+          />
+          <label htmlFor="age">Mam co najmniej 16 lat</label>
+          <br />
+          <button>Kup bilet</button>
+        </form>
         {this.displayMessage()}
       </>
-    )
+    );
   }
 }
 
-ReactDOM.render(<ToggleCheckbox />, document.getElementById('root'));
+ReactDOM.render(<ToggleCheckbox />, document.getElementById("root"));
